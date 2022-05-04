1dc8e42b5c5b924093ff0aa472db232875b234d7
Cole <colescarter@gmail.com>
2022-05-04 15:26:17 -0700

    Added Test file and updated skillsDemo.java

--- skillsDemonstration.java
+++ skillsDemonstration.java
@@ -1,4 +1,10 @@
 class SkillsDemonstration{
 
+        public static int divide(int i, int j){
+            if(j == 0){
+                throw new IllegalArgumentException();
+            }
+            return i/j;
+        }
 
 }
---
+++ skillsDemonstrationTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillsDemonstrationTester {
+    @Test
+    public void divideTester(){
+        assertEquals(1, SkillsDemonstration.divide(1,2));
+    }
+}
